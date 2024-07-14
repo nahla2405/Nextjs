69bdbca3e5bdb5021c4d17e09dc20708818304f8
nahla2405 <nahlathayeeb@gmail.com>
2024-07-14 21:08:11 +0400

    new file:   next_prac/.eslintrc.json 	new file:   next_prac/.gitignore 	new file:   next_prac/README.md 	new file:   next_prac/app/about/page.jsx 	new file:   next_prac/app/cart/page.jsx 	new file:   next_prac/app/checkout/page.jsx 	new file:   next_prac/app/favicon.ico 	new file:   next_prac/app/globals.css 	new file:   next_prac/app/layout.jsx 	new file:   next_prac/app/login/page.jsx 	new file:   next_prac/app/page.jsx 	new file:   next_prac/app/post/[id]/page.jsx 	new file:   next_prac/app/products/[id]/page.jsx 	new file:   next_prac/app/products/page.jsx 	new file:   next_prac/app/profile/page.jsx 	new file:   next_prac/app/register/page.jsx 	new file:   next_prac/jsconfig.json 	new file:   next_prac/next.config.mjs 	new file:   next_prac/package-lock.json 	new file:   next_prac/package.json 	new file:   next_prac/postcss.config.mjs 	new file:   next_prac/public/next.svg 	new file:   next_prac/public/vercel.svg 	new file:   next_prac/tailwind.config.jsx 	new file:   practice1/app/post/[id]/page.jsx

---
+++ next_prac/app/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <h1 className="text-4xl font-bold text-center text-blue-600 mb-4">Nahla</h1>
+    <p className="text-lg text-gray-700 bg-yellow-200 p-4 rounded shadow-md hover:bg-yellow-300 transition-all">
+      Hi there, my name is Nahla and i feel that is a brief enough intro of myself.
+    </p>
+    <p className="text-md text-white bg-gradient-to-r from-purple-400 to-pink-500 p-6 m-2 rounded-lg text-center shadow-lg">
+      So far the task are easy and doable. Best part is the simplicity paired with the learning we can learn a lot with jus a few simple clicks, which is good as people will be ready to spend a few minutes of there time everyday jus to interact and actually complete it. if each task is complex and time consuming students may tend to loose interest easily and rush everything before the deadline while still not learning anything as they will be focused on the deadline rather than the learning.
+    </p>
+    </>
+  )
+}
+
+export default page
---
+++ next_prac/app/register/page.jsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+
+const RegisterPage = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center text-green-600 mb-3">
+        Register
+      </h1>
+      <p className="text-lg text-gray-700 p-4">
+        Create an account to start shopping.
+      </p>
+    </>
+  );
+};
+
+export default RegisterPage;
